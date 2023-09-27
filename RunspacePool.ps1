946717ec8b592aa63ceb435644c8ccdce4423f74
Elazar Levin <levine@legal.regn.net>
2023-09-27 12:12:55 +0200

    fixed runspace pools

--- RunspacePool.ps1
+++ RunspacePool.ps1
@@ -1,6 +1,9 @@
-$Scriptblock = {
+$ScriptBlock = {
     param($Name)
-    New-Item -Name $Name -ItemType File
+	Write-Host $Name
+	Write-Host $pwd
+	Set-Location C:\dev\Powershell Example Scripts
+    New-Item -Path "C:\dev\Powershell Example Scripts\" -Name "$Name.txt" -ItemType File
 }
 $MaxThreads = 5
 $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads)
@@ -10,9 +13,13 @@ $Jobs = @()
 	$PowerShell = [powershell]::Create()
 	$PowerShell.RunspacePool = $RunspacePool
 	$PowerShell.AddScript($ScriptBlock).AddArgument($_)
-	$Jobs += $PowerShell.BeginInvoke()
+	$Jobs += @{powershell=$PowerShell;job=$PowerShell.BeginInvoke()}
 }
 
-while ($Jobs.IsCompleted -contains $false) {
+while ($Jobs.job.IsCompleted -contains $false) {
 	Start-Sleep 1
+}
+
+foreach($Job in $Jobs){
+	$Job.powershell.EndInvoke($Job.job)
 }
